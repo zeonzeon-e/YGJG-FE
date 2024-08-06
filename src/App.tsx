49f25932008b3ad8e918322d3908579250a075e3
ZEONe <julia3471@gmail.com>
2024-08-06 18:02:42 +0900

    ScrollProgress 컴포넌트 기능 구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import Calendar from "./component/Calendar/Calendar";
 import TeamJoinHeader from "./component/Header/TeamJoinHeader/TeamJoinHeader";
 import Header2 from "./component/Header/Header2/Header2";
+import ScrollProgress from "./component/ScrollProgress/ScrollProgress";
 
 function App() {
   return (
     <div className="App">
       <Header2 text="팀 경기 일정" line={true} />
       <Calendar />
+      <ScrollProgress targetWidth={15} duration={10} />
     </div>
   );
 }
--- src/component/ScrollProgress/ScrollProgress.tsx
+++ src/component/ScrollProgress/ScrollProgress.tsx
@@ -1,34 +1,45 @@
-import React from "react";
-import { FaThumbtack } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
 import "./ScrollProgress.css";
 
 interface ScrollProgressProps {
-  title: string;
-  date: string;
-  children?: React.ReactNode;
+  targetWidth?: number; //진행율
+  duration: number; //애니메이션 지속 시간(ms)
 }
 
 /**
  * ScrollProgress 컴포넌트 - 진행과정 렌더링
  * @param {ScrollProgressProps} props - 컴포넌트에 전달되는 props
- * @param {React.ReactNode} [props.children] - 공지사항의 내용 (선택적)
+ * @param {React.ReactNode} [props.targetWidth] - 컴포넌트 진행도
  * @returns {JSX.Element} ScrollProgress 컴포넌트
  */
 const ScrollProgress: React.FC<ScrollProgressProps> = ({
-  title,
-  date,
-  children,
+  targetWidth = 0,
+  duration,
 }) => {
+  const [width, setWidth] = useState<number>(0);
+
+  useEffect(() => {
+    let animationFrame: number;
+    const start = performance.now();
+    const animate = (time: number) => {
+      const elapsedTime = time - start;
+      const progress = Math.min(elapsedTime / duration, 1);
+      setWidth(progress * targetWidth);
+
+      if (progress < 1) {
+        animationFrame = requestAnimationFrame(animate);
+      }
+    };
+    animationFrame = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(animationFrame);
+  }, [targetWidth, duration]);
+
   return (
-    <div className="notice-card">
-      <div className="notice-card-header">
-        <div className="notice-card-title">
-          <FaThumbtack className="notice-card-icon" />
-          {title}
-        </div>
-        <div className="notice-card-date">{date} 수정</div>
-      </div>
-      <div className="notice-card-content">{children}</div>
+    <div className="ScrollProgress">
+      <div
+        className="Progress"
+        style={{ width: width + "%", transition: "width 0.5s ease-in-out" }}
+      ></div>
     </div>
   );
 };
