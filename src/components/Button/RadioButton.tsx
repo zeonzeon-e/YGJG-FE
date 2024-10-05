1667d61568558d1c3247cd74e900c72e290bda55
sleepyMS <minseok011017@gmail.com>
2024-10-06 01:40:38 +0900

    라디오버튼 기능 업데이트 및 회원가입 페이지 기능구현

--- src/components/Button/RadioButton.tsx
+++ src/components/Button/RadioButton.tsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+/*
+  사용 예시
+  const [gender, setGender] = useState<string | null>(null);
+
+  
+  <RadioButton
+    items={["남성", "여성"]}
+    onChange={(value) => setGender(value)} // 선택된 성별 값을 설정
+  />
+*/
+
 /**
  * RadioButton 컴포넌트 - 단일 선택이 가능한 라디오 버튼을 렌더링합니다.
  * @param {RadioButtonProps} props - 컴포넌트에 전달되는 props
@@ -10,6 +21,7 @@ import styled from "styled-components";
  * @property {number} [fontSize] - 텍스트 사이즈 (선택적)
  * @property {string} [bgColor] - 선택되지 않은 버튼의 배경색 (선택적)
  * @property {string} [selectedBgColor] - 선택된 버튼의 배경색 (선택적)
+ * @property {(value: string) => void} onChange - 선택된 값을 부모 컴포넌트로 전달하는 함수
  * @returns {JSX.Element} RadioButton 컴포넌트
  */
 const RadioButton: React.FC<RadioButtonProps> = ({
@@ -19,16 +31,17 @@ const RadioButton: React.FC<RadioButtonProps> = ({
   fontSize,
   bgColor,
   selectedBgColor,
+  onChange,
 }) => {
-  // 선택된 버튼의 인덱스를 관리하는 내부 state
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
   /**
-   * handleButtonClick - 라디오 버튼 클릭 시 선택 상태를 업데이트하는 함수
+   * handleButtonClick - 라디오 버튼 클릭 시 선택 상태를 업데이트하고 부모 컴포넌트에 선택된 값을 전달하는 함수
    * @param {number} index - 클릭된 버튼의 인덱스
    */
   const handleButtonClick = (index: number) => {
     setSelectedIndex(index);
+    onChange && onChange(items[index]); // 선택된 값을 부모 컴포넌트로 전달
   };
 
   return (
@@ -57,16 +70,16 @@ export default RadioButton;
 
 // 인터페이스 정의
 interface RadioButtonProps {
-  title?: string; // 컴포넌트의 제목 (선택적)
+  title?: string;
   items: string[];
   textColor?: string;
   fontSize?: number;
-  bgColor?: string; // 선택되지 않은 버튼의 배경색 (선택적)
-  selectedBgColor?: string; // 선택된 버튼의 배경색 (선택적)
+  bgColor?: string;
+  selectedBgColor?: string;
+  onChange?: (value: string) => void;
 }
 
 // 스타일 컴포넌트 정의
-
 const Container = styled.div`
   width: 100%;
 `;
@@ -78,7 +91,6 @@ const Title = styled.h3`
   color: var(--color-dark2);
 `;
 
-// 버튼들을 담는 컨테이너 (Flexbox 레이아웃)
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: space-between;
@@ -93,12 +105,11 @@ interface RadioItemButtonProps {
   selectedBgColor?: string;
 }
 
-// 개별 라디오 버튼의 스타일
 const RadioItemButton = styled.button<RadioItemButtonProps>`
   border: 1px solid var(--color-dark1);
   padding: 10px;
   margin: 4px;
-  border-radius: 8px; /* 기본적으로 8px의 border-radius */
+  border-radius: 8px;
   color: ${({ isSelected, textColor }) =>
     isSelected ? "var(--color-light1)" : textColor || "var(--color-main)"};
   background-color: ${({ isSelected, bgColor, selectedBgColor }) =>
