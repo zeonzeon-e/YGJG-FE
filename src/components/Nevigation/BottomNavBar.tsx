621eba5fbe0e30c5c249cbab4e3a5c0fed3b9108
sleepyMS <minseok011017@gmail.com>
2025-07-21 10:27:04 +0900

    Navigation Update
    기초 네비게이션 (하단 가림문제 해결 필요)

--- src/components/Nevigation/BottomNavBar.tsx
+++ src/components/Nevigation/BottomNavBar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaHome, FaUsers, FaUser } from "react-icons/fa";
+import { useNavigate } from "react-router-dom"; // useNavigate 훅 임포트
 import "./BottomNavBar.css";
 
 interface NavItemType {
@@ -13,26 +14,33 @@ interface NavItemType {
 const BottomNavBar: React.FC = () => {
   // 현재 선택된 네비게이션 항목 (기본값: "home")
   const [selected, setSelected] = useState<NavItemType["value"]>("home");
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  // 네비게이션 아이템 클릭 핸들러
+  const handleItemClick = (itemValue: NavItemType["value"], path: string) => {
+    setSelected(itemValue); // 선택된 상태 업데이트
+    navigate(path); // 해당 경로로 이동
+  };
 
   return (
     <div className="nav-bar">
       <div
         className={`nav-item ${selected === "home" ? "selected" : ""}`}
-        onClick={() => setSelected("home")}
+        onClick={() => handleItemClick("home", "/myteam")}
       >
         <FaHome size={24} />
         <span className="icon-label">Home</span>
       </div>
       <div
         className={`nav-item ${selected === "users" ? "selected" : ""}`}
-        onClick={() => setSelected("users")}
+        onClick={() => handleItemClick("users", "/team/list")}
       >
         <FaUsers size={24} />
         <span className="icon-label">Users</span>
       </div>
       <div
         className={`nav-item ${selected === "profile" ? "selected" : ""}`}
-        onClick={() => setSelected("profile")}
+        onClick={() => handleItemClick("profile", "/my")}
       >
         <FaUser size={24} />
         <span className="icon-label">Profile</span>
